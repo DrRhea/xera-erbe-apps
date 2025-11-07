2ba7c9c5fe155aa39f3a1e2fc8f3a917425ae33a
Lyan Nazhabil Dzuquwwa <140893531+Ilyessin12@users.noreply.github.com>
2025-11-08 01:32:25 +0700

    fix : homescreen root stack because we dont use login for now

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
 import HomeScreen from './src/screens/home/HomeScreen';
 import TryoutScreen from './src/screens/tryout/TryoutScreen';
 import TryoutDetailScreen from './src/screens/tryout/TryoutDetailScreen';
@@ -142,10 +143,14 @@ const navigationTheme = {
 };
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hideAsync().catch(() => null);
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer theme={navigationTheme}>
-        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
